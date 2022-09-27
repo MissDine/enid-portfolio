a0232481e29c8278c9e3bcd852743743838042b0
MissDine <enidnyatichi1212@gmail.com>
2022-09-27 20:45:51 +0300

    Feature edit

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -6,20 +6,24 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-      <Navbar className='navba' collapseOnSelect expand="lg" variant="dark">
+      <Navbar className='col-sm-12' collapseOnSelect expand="lg" variant="dark">
       <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
+          <div className="col-sm-12">
           <Nav className="col-sm-12">
-            <Nav.Link href="#facebook"><img src="https://img.icons8.com/ios-glyphs/2x/facebook.png" alt="facebook" /></Nav.Link>
-            <Nav.Link href="#instagram"><img src="https://img.icons8.com/ios-filled/2x/instagram-new.png" alt="instagram" /></Nav.Link>
-            <Nav.Link href="#twitter"><img src="https://img.icons8.com/ios-filled/2x/twitter-squared.png" alt="twitter" /></Nav.Link>
-            <Nav.Link href="#facebook"><img src="https://img.icons8.com/external-prettycons-flat-prettycons/2x/external-youtube-multimedia-prettycons-flat-prettycons.png" alt="youtube" /></Nav.Link>
-            <Nav.Link as = {Link} to = {"/home"}>Home</Nav.Link>
-            <Nav.Link as = {Link} to = {"/chaplaincy"}>Chaplaincy</Nav.Link>
-            <Nav.Link as = {Link} to = {"/contact"}>Contact Us</Nav.Link>
-            <Nav.Link as = {Link} to = {"/support"}>Support St Paul's</Nav.Link>
+            <Nav.Link href="#facebook"><img src="https://img.icons8.com/ios-glyphs/2x/facebook.png" alt="facebook" style={{height:"40px",width:"40px"}}/></Nav.Link>
+            <Nav.Link href="#instagram"><img src="https://img.icons8.com/ios-filled/2x/instagram-new.png" alt="instagram" style={{height:"40px",width:"40px"}}/></Nav.Link>
+            <Nav.Link href="#twitter"><img src="https://img.icons8.com/ios-filled/2x/twitter-squared.png" alt="twitter" style={{height:"40px",width:"40px"}}/></Nav.Link>
+            <Nav.Link href="#facebook"><img src="https://img.icons8.com/external-prettycons-flat-prettycons/2x/external-youtube-multimedia-prettycons-flat-prettycons.png" alt="youtube" style={{height:"40px",width:"40px"}}/></Nav.Link>
+            <Nav.Link as = {Link} to = {"/home"} style={{textDecoration:"none",marginLeft:"10vw"}}>Home</Nav.Link>
+            <Nav.Link as = {Link} to = {"/About"} style={{textDecoration:"none"}}>About</Nav.Link>
+            <Nav.Link as = {Link} to = {"/Services"} style={{textDecoration:"none"}}>Services</Nav.Link>
+            <Nav.Link as = {Link} to = {"/experiences"} style={{textDecoration:"none"}}>experiences</Nav.Link>
+            <Nav.Link as = {Link} to = {"/Projects"} style={{textDecoration:"none"}}>Projects</Nav.Link>
+            <Nav.Link as = {Link} to = {"/Contact"} style={{textDecoration:"none"}}>Contact</Nav.Link>
           </Nav>
+          </div>
         </Navbar.Collapse>
       </Container>
       </Navbar>
